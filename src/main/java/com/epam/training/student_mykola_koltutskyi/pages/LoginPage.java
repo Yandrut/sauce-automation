90b6a0d01253f9b12196f6620fae4307e49d415c
Yandrut <nickolayko@yahoo.com>
2024-10-28 00:32:36 +0200

    testing related files and dependencies added

--- src/main/java/com/epam/training/student_mykola_koltutskyi/pages/LoginPage.java
+++ src/main/java/com/epam/training/student_mykola_koltutskyi/pages/LoginPage.java
@@ -1,12 +1,11 @@
 package com.epam.training.student_mykola_koltutskyi.pages;
 
-import com.epam.training.student_mykola_koltutskyi.models.User;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LoginPage extends AbstractPage {
 
@@ -22,7 +21,7 @@ public class LoginPage extends AbstractPage {
     @FindBy(css = "div.error-message-container.error > h3")
     private WebElement errorMessage;
 
-    private static final Logger log = LogManager.getLogger(LoginPage.class);
+    private static final Logger log = LoggerFactory.getLogger(LoginPage.class);
     private final WebDriver driver;
 
     public LoginPage(WebDriver driver) {
@@ -37,38 +36,34 @@ public class LoginPage extends AbstractPage {
         return this;
     }
 
-    public LoginPage typeCredentials(User user) {
+    public void typeCredentials(String username, String password) {
         Actions actions = new Actions(driver);
-        actions.sendKeys(usernameField, user.getUsername())
-                .sendKeys(passwordField, user.getPassword())
+        actions.sendKeys(usernameField, username)
+                .sendKeys(passwordField, password)
                 .build().perform();
-        log.info("Typing the user credentials");
-        return this;
     }
 
-    public LoginPage clearPasswordInput() {
+    public void clearPasswordInput() {
         Actions actions = new Actions(driver);
         actions.doubleClick(passwordField)
                 .sendKeys(Keys.BACK_SPACE)
                 .build()
                 .perform();
-
-        return this;
+        log.info("Password input cleared");
     }
 
-    public LoginPage clearUsernameInput() {
+    public void clearUsernameInput() {
         Actions actions = new Actions(driver);
         actions.doubleClick(usernameField)
                 .sendKeys(Keys.BACK_SPACE)
                 .build()
                 .perform();
-        return this;
+        log.info("Username input cleared");
     }
 
-    public LoginPage clickOnLoginButton() {
+    public void clickOnLoginButton() {
         loginButton.submit();
         log.info("Clicking on login button");
-        return this;
     }
 
     public String getErrorMessage() {
