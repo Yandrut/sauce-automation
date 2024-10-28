f9ca39160cd7e68942fcd91394c1bd687018dadc
Yandrut <nickolayko@yahoo.com>
2024-10-28 14:45:56 +0200

    fixing issues

--- src/main/java/com/epam/training/student_mykola_koltutskyi/pages/AbstractPage.java
+++ src/main/java/com/epam/training/student_mykola_koltutskyi/pages/AbstractPage.java
@@ -2,7 +2,5 @@ package com.epam.training.student_mykola_koltutskyi.pages;
 
 public abstract class AbstractPage {
 
-    public abstract AbstractPage navigateToUrl(String url);
-
     public abstract String getTitle();
 }
